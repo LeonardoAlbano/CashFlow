0130811c048150155aff8fccbf4c28a54c4c0c61
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2025-03-21 11:02:56 -0300

    DI injection dependency

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,33 +1,40 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Exception.ExceptionsBase;
-using CashFlow.Infrastructure.DataAccess;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
-public class RegisterExpenseUseCase
+public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
+    private readonly IExpensesRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RegisterExpenseUseCase(IExpensesRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
     public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
         Validate(request);
 
-        var dbContext = new CashFlowDbContext();
-
         var entity = new Expense
         {
             Amount = request.Amount,
-            PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
             Date = request.Date,
-            Title = request.Title,
             Description = request.Description,
+            Title = request.Title,
+            PaymentType = (Domain.Enums.PaymentType)request.PaymentType,
         };
-        
-        dbContext.Expenses.Add(entity);
-        
-        dbContext.SaveChanges();
+
+        _repository.Add(entity);
+
+        _unitOfWork.Commit();
 
         return new ResponseRegisterExpenseJson();
-
     }
 
     private void Validate(RequestRegisterExpenseJson request)
--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -2,19 +2,9 @@ using CashFlow.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess;
-
-public class CashFlowDbContext : DbContext
+internal class CashFlowDbContext : DbContext
 {
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
     public DbSet<Expense> Expenses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=Hety8a6yqe!;";
-
-        var version = new Version(9,2,0);
-        
-        var serverVersion = new MySqlServerVersion(version);
-        
-        optionsBuilder.UseMySql(connectionString, serverVersion );
-    }
 }
