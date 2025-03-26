aa81ec0e58ebc770306f562dd2669ab04ec59b63
leonardoalbano <leonardoalbanom@yahoo.com.br>
2025-03-25 22:32:57 -0300

    ff

--- tests/CommonTestUtilities/obj/Debug/net8.0/CommonTestUtilities.AssemblyInfo.cs
+++ tests/CommonTestUtilities/obj/Debug/net8.0/CommonTestUtilities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonTestUtilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13250fea28e32f164396257cf04ad9aa841c43f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonTestUtilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonTestUtilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
